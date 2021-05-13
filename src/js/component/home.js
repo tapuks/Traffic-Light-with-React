82e96f917a7594b504a2ad3bde2403bb235a9e29
tapuks <david_berdiell@hotmail.com>
2021-05-13 19:39:50 +0000

    edit colors

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
-	return <div className="text-center mt-5">fdfdf</div>;
+	const [color1, setColor1] = useState("iluminated");
+	const [color2, setColor2] = useState("yellow");
+	const [color3, setColor3] = useState("greenLight");
+	const clickRed = () => {
+		console.log("red");
+	};
+	return (
+		<div className="trafficLight">
+			<button className="redLight" onClick={() => setColor({})}></button>
+			<button className="yellowLight" onClick={() => setColor()}></button>
+			<button
+				className={color3}
+				onClick={() => setColor3("iluminated")}></button>
+		</div>
+	);
 }
