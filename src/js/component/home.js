24111111e7bcc4bbdbb1d1a7676594c7a9bfec99
tapuks <david_berdiell@hotmail.com>
2021-05-15 15:57:32 +0000

    exercise finished

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,18 +3,32 @@ import React, { useState } from "react";
 //create your first component
 export function Home() {
 	const [color1, setColor1] = useState("iluminated");
-	const [color2, setColor2] = useState("yellow");
+	const [color2, setColor2] = useState("yellowLight");
 	const [color3, setColor3] = useState("greenLight");
-	const clickRed = () => {
-		console.log("red");
-	};
+
 	return (
 		<div className="trafficLight">
-			<button className="redLight" onClick={() => setColor({})}></button>
-			<button className="yellowLight" onClick={() => setColor()}></button>
+			<button
+				className={color1}
+				onClick={() => {
+					setColor1("iluminated");
+					setColor2("yellowLight");
+					setColor3("greenLight");
+				}}></button>
+			<button
+				className={color2}
+				onClick={() => {
+					setColor2("iluminated");
+					setColor1("redLight");
+					setColor3("greenLight");
+				}}></button>
 			<button
 				className={color3}
-				onClick={() => setColor3("iluminated")}></button>
+				onClick={() => {
+					setColor3("iluminated");
+					setColor2("yellowLight");
+					setColor1("redLight");
+				}}></button>
 		</div>
 	);
 }
